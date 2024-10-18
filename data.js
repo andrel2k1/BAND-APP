1e7b7d990ec8f83e6ba741ff2f463d1844bef8c8
andrel2k1 <andre_oliver_1990@hotmail.com>
2024-10-18 19:04:33 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,89 @@
+let songs = [
+
+    {
+        name:'Generation',
+        path: 'music/Generation.mp3',
+        artist: 'Medicine X',
+        cover: 'img/gen.png'
+
+
+    },
+
+    {
+        name:'Fight for a Future',
+        path: 'music/Medicine X  Fight for a Future.mp3',
+        artist: 'Medicine X',
+        cover: 'img/warrior.png'
+
+
+    },
+    {
+        name:'Loyalty and Friendship',
+        path: 'music/Medicine X  Loyalty and Friendship.mp3',
+        artist: 'Medicine X',
+        cover: 'img/7.jpg'
+
+
+    },
+
+    {
+        name:'Rebellious Hearts',
+        path: 'music/Medicine X  Rebellious Hearts.mp3',
+        artist: 'Medicine X',
+        cover: 'img/circle.png'
+
+
+    },
+    {
+        name:'9to5Blues',
+        path: 'music/Medicine X  9to5Blues.mp3',
+        artist: 'Medicine X',
+        cover: 'img/1.jpg'
+
+
+    },
+    {
+        name:'Enough for the shameful lie',
+        path: 'music/ReelAudio-97942.mp3',
+        artist: 'Medicine X',
+        cover: 'img/Design sem nome (6).png'
+
+
+    },
+    {
+        name:'Thinking about the other side',
+        path: 'music/Medicine X - Thinking about the other side.mp3',
+        artist: 'Medicine X',
+        cover: 'img/Design sem nome (3).png'
+
+
+    },
+    {
+        name:'Another soul',
+        path: 'music/Medicine X - Another soul.mp3',
+        artist: 'Medicine X',
+        cover: 'img/10.jpeg'
+
+
+    },
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
